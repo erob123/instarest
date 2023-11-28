2eb8e7d87b39b6f4c7046b910e3e28a71738a3c1
erob123 <46288978+erob123@users.noreply.github.com>
2023-11-28 07:40:20 -0500

    Feat/pgvector (#13)
    * pgvector works w/insta updates, init WIP
    
    * init WIP
    
    * init works, full pgvector done

--- instarest/initializer.py
+++ instarest/initializer.py
@@ -1,8 +1,11 @@
+import traceback
+from sqlalchemy.sql import text
+from sqlalchemy.orm import Session
 from instarest.db.base_class import DeclarativeBase
 from instarest.db.init_db import init_db, wipe_db
-from instarest.db.session import SessionLocal
 from instarest.core.config import get_environment_settings
 from instarest.core.logging import LogConfig
+from instarest.db.session import SessionLocal
 
 
 class Initializer:
@@ -16,7 +19,17 @@ class Initializer:
     def wipe_db(self):
         wipe_db(self.Base)
 
-    def execute(self, migration_toggle=False) -> None:
+    def init_vector_db(self, db: Session) -> None:
+        try:
+            db.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))  # for pgvector
+            db.commit()
+        except Exception as err:
+            self.logger.error(err)
+            traceback.print_exc()
+            db.rollback()
+            raise err
+
+    def execute(self, migration_toggle=False, vector_toggle=False) -> None:
         # environment can be one of 'local', 'development, 'test', 'staging', 'production'
         environment = get_environment_settings().environment
 
@@ -29,13 +42,24 @@ class Initializer:
             self.wipe_db()
             self.logger.info("Database cleared")
 
+        # setup vector db if desired
+        if vector_toggle:
+            self.logger.info("Connecting DB (InstarestInitializer)")
+            db = SessionLocal()
+            self.logger.info("DB connected (InstarestInitializer)")
+
+            self.logger.info("Ensuring Vector extension is enabled in DB")
+            self.init_vector_db(db)
+            self.logger.info("Vector extension enabled in DB")
+
+            db.close()
+
         # all environments need to initialize the database
         # prod only if migration toggle is on
         if environment in ["local", "development", "test", "staging"] or (
             environment == "production" and migration_toggle is True
         ):
             self.logger.info("Creating database schema and tables")
-            db = SessionLocal()
             self.init_db()
             self.logger.info("Initial database schema and tables created.")
         else:
--- instarest/utils/models.py
+++ instarest/utils/models.py
@@ -1,6 +1,8 @@
 import inspect as ins
-from pydantic import UUID4
-from typing import TYPE_CHECKING, Any, Optional, Generator
+from pydantic import UUID4, validator
+from typing import Any, Generator
+from datetime import datetime
+from enum import Enum as CoreEnum
 from sqlalchemy import (
     Boolean,
     DateTime,
@@ -11,6 +13,14 @@ from sqlalchemy import (
 )
 from instarest.db.base_class import DeclarativeBase
 
+# support for pgvector without requiring it to be installed in instarest
+try:
+    from pgvector.sqlalchemy import Vector
+    from numpy import ndarray
+except ImportError:
+    Vector = None
+    ndarray = None
+
 
 def remove_endstr(str: str, sub: str) -> str:
     if str.endswith(sub):
@@ -20,7 +30,7 @@ def remove_endstr(str: str, sub: str) -> str:
 
 def gen_column_attrs(
     ModelType: DeclarativeBase,
-) -> Generator[tuple[str, tuple[type | Any, ...]], None, None]:
+) -> Generator[tuple[str, tuple[type | Any, ...], dict[str, Any]], None, None]:
     """
     Generator for pulling attributes and types from the declared model.
     Includes foreign keys if actually saved in the table, but does not
@@ -37,7 +47,7 @@ def gen_column_attrs(
     pydantic schema.  "..." indicates no default in the schema.
     """
     mapper = inspect(ModelType)
-
+    validators = {}
     for column in mapper.column_attrs:
         # name of the attribute, will be returned
         field_name = column.key
@@ -53,14 +63,41 @@ def gen_column_attrs(
         if isinstance(field.expression.type, Boolean):
             field_type = bool
 
-        yield field_name, (field_type, ...)
+        if isinstance(field.expression.type, DateTime):
+            field_type = datetime
+
+        if isinstance(field.expression.type, String):
+            field_type = str
+
+        if isinstance(field.expression.type, Enum):
+            field_type = CoreEnum
+
+        if (
+            Vector is not None
+            and ndarray is not None
+            and isinstance(field.expression.type, Vector)
+        ):
+            field_type = list[float]
+
+            def numpy_to_list(cls, v):
+                if isinstance(v, ndarray):
+                    return v.tolist()
+                return v
+
+            validators[f"{field_name}_numpy_validator"] = validator(
+                field_name, pre=True, always=True, allow_reuse=True
+            )(numpy_to_list)
+
+        yield field_name, (field_type, ...), validators
 
 
 def dict_column_attrs_with_id(
-    ModelType: DeclarativeBase,
-) -> dict[str, tuple[type | Any, ...]]:
+    ModelType: DeclarativeBase, optional_fields: list[str] = []
+) -> tuple[dict[str, tuple[type | Any | None, ...]], dict[str, Any]]:
     """
-    Dictionary that gives same values as gen_column_attrs
+    Dictionary that gives same values as gen_column_attrs.
+    If OptionalFields is provided, then those fields will be
+    made optional in the output dictionary.
 
     **Parameters**
 
@@ -71,16 +108,22 @@ def dict_column_attrs_with_id(
     dict generated from gen_column_attrs
     """
 
-    output = {}
-    for field_name, (field_type, _) in gen_column_attrs(ModelType):
-        output[field_name] = (field_type, ...)
+    output_fields = {}
+    output_validators = {}
+    for field_name, (field_type, _), validators in gen_column_attrs(ModelType):
+        if field_name in optional_fields:
+            output_fields[field_name] = (field_type | None, None)
+        else:
+            output_fields[field_name] = (field_type, ...)
 
-    return output
+        output_validators.update(validators)
+
+    return output_fields, output_validators
 
 
 def dict_optional_column_attrs_no_id(
     ModelType: DeclarativeBase,
-) -> dict[str, tuple[type | Any | None, ...]]:
+) -> tuple[dict[str, tuple[type | Any | None, ...]], dict[str, Any]]:
     """
     Dictionary that gives same values as gen_column_attrs,
     except that every field_type is made optional (for use
@@ -97,12 +140,14 @@ def dict_optional_column_attrs_no_id(
     default value for pydantic schemas
     """
 
-    output = {}
-    for field_name, (field_type, _) in gen_column_attrs(ModelType):
+    output_fields = {}
+    output_validators = {}
+    for field_name, (field_type, _), validators in gen_column_attrs(ModelType):
         if field_name != "id":
-            output[field_name] = (field_type | None, None)
+            output_fields[field_name] = (field_type | None, None)
+            output_validators.update(validators)
 
-    return output
+    return output_fields, output_validators
 
 
 def gen_relationship_attrs(
@@ -170,11 +215,3 @@ def dict_relationship_attrs(
         output[field_name] = (field_type, ...)
 
     return output
-
-
-# for field_name, (field_type, _) in gen_column_attrs(DeclarativeBase):
-#     print(field_name, field_type)
-
-
-# for field_name, (field_type, _) in gen_relationship_attrs(DeclarativeBase, ins.getmembers(schemas)):
-#     print(field_name, field_type)
