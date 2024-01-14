c7c3952f96394ca89e5e31ba7ef2f2f0f9807648
erob123 <ejr443@gmail.com>
2024-01-14 15:14:18 -0500

    fix ssl setup

--- instarest/db/session.py
+++ instarest/db/session.py
@@ -1,14 +1,14 @@
-import ssl
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from instarest.core.config import get_core_settings
 
-if get_core_settings().db_cert_path is None:
+if get_core_settings().db_rootcert_path is None:
     connect_args = {}
 else:
-    ssl_context = ssl.create_default_context(cafile=get_core_settings().db_cert_path)
-    ssl_context.verify_mode = ssl.CERT_REQUIRED
-    connect_args = {"ssl": ssl_context}
+    connect_args = {
+        "sslrootcert": get_core_settings().db_rootcert_path,
+        "sslmode": "verify-full",
+    }
 
 engine = create_engine(
     get_core_settings().sqlalchemy_database_uri,
